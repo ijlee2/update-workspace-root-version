7da0beafd7899197ceabba4a8daf2c93a21ee1d2
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-28 14:29:31 +0200

    feature: Found package versions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { createOptions } from './steps/index.js';
+import { createOptions, getPackageVersions } from './steps/index.js';
 import type { CodemodOptions } from './types/index.js';
 
 export function runCodemod(codemodOptions: CodemodOptions): void {
   const options = createOptions(codemodOptions);
 
-  // TODO: Replace with actual steps
-  console.log(options);
+  const packageVersions = getPackageVersions(options);
+
+  console.log(packageVersions);
 }
--- src/steps/index.ts
+++ src/steps/index.ts
@@ -1,1 +1,2 @@
 export * from './create-options.js';
+export * from './get-package-versions.js';
