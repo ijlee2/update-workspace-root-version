6be319217c3ffea85b0ea5826aabea04937c8056
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-29 07:44:58 +0200

    refactor: Added tests (get-new-version)

---
+++ tests/steps/get-new-version/highest-version.test.ts
@@ -0,0 +1,33 @@
+import { assert, test } from '@codemod-utils/tests';
+
+import { getNewVersion } from '../../../src/steps/index.js';
+import { options } from '../../helpers/shared-test-setups/monorepo-highest-version-1.js';
+
+test('steps | get-new-version > highest-version', function () {
+  assert.strictEqual(
+    getNewVersion(
+      ['0.3.1', '0.4.0', '0.2.5', '0.3.4', '0.2.0'],
+      '0.4.0',
+      options,
+    ),
+    '0.4.1',
+  );
+
+  assert.strictEqual(
+    getNewVersion(
+      ['0.3.1', '0.5.0', '0.2.5', '0.3.3', '0.1.8'],
+      '0.4.0',
+      options,
+    ),
+    '0.5.0',
+  );
+
+  assert.strictEqual(
+    getNewVersion(
+      ['1.8.13', '2.5.0', '2.4.1', '3.0.0', '0.9.3'],
+      '2.4.1',
+      options,
+    ),
+    '3.0.0',
+  );
+});
