36429d59c2198f8c490c94200e82eb968e4a015c
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-02-24 13:08:11 +0100

    Updated dependencies (#15)
    * chore: Updated dependencies
    
    * chore: Updated lockfile
    
    * refactor: Consumed getPackageRoots() from @codemod-utils/files
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- src/steps/get-package-versions.ts
+++ src/steps/get-package-versions.ts
@@ -1,30 +1,8 @@
-import { join } from 'node:path';
-
-import { findFiles } from '@codemod-utils/files';
+import { getPackageRoots } from '@codemod-utils/files';
 import { readPackageJson } from '@codemod-utils/json';
 
 import type { Options } from '../types/index.js';
 import { allow } from '../utils/version/index.js';
-
-function getPackageRoots(options: Options): string[] {
-  const { projectRoot } = options;
-
-  const packageRoots = findFiles('**/package.json', {
-    ignoreList: ['**/{dist,node_modules}/**/*'],
-    projectRoot,
-  }).map((filePath) => {
-    return join(projectRoot, filePath.replace(/package\.json$/, ''));
-  });
-
-  const isMonorepo = packageRoots.length > 1;
-
-  if (isMonorepo) {
-    // Remove the workspace root
-    return packageRoots.filter((packageRoot) => packageRoot !== projectRoot);
-  }
-
-  return packageRoots;
-}
 
 export function getPackageVersions(options: Options): string[] {
   const packageRoots = getPackageRoots(options);
