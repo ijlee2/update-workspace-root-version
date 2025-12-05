ee7994a752a661f421c56691ec583bab2395c473
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-05 08:08:41 -0600

    refactor: Removed unnecessary normalize()'s

--- tests/helpers/shared-test-setups/monorepo-increment-by-one-1.ts
+++ tests/helpers/shared-test-setups/monorepo-increment-by-one-1.ts
@@ -1,15 +1,13 @@
-import { normalize } from 'node:path';
-
 import type { CodemodOptions, Options } from '../../../src/types/index.js';
 
 const codemodOptions: CodemodOptions = {
   algorithm: 'increment-by-one',
-  projectRoot: normalize('tmp/monorepo-increment-by-one-1'),
+  projectRoot: 'tmp/monorepo-increment-by-one-1',
 };
 
 const options: Options = {
   algorithm: 'increment-by-one',
-  projectRoot: normalize('tmp/monorepo-increment-by-one-1'),
+  projectRoot: 'tmp/monorepo-increment-by-one-1',
 };
 
 export { codemodOptions, options };
--- tests/helpers/shared-test-setups/not-monorepo.ts
+++ tests/helpers/shared-test-setups/not-monorepo.ts
@@ -1,15 +1,13 @@
-import { normalize } from 'node:path';
-
 import type { CodemodOptions, Options } from '../../../src/types/index.js';
 
 const codemodOptions: CodemodOptions = {
   algorithm: 'highest-version',
-  projectRoot: normalize('tmp/not-monorepo'),
+  projectRoot: 'tmp/not-monorepo',
 };
 
 const options: Options = {
   algorithm: 'highest-version',
-  projectRoot: normalize('tmp/not-monorepo'),
+  projectRoot: 'tmp/not-monorepo',
 };
 
 export { codemodOptions, options };
